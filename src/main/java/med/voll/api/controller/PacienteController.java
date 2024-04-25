a8bcc2effbcfddc48c6642e9526a11e60ad20ba7
Pedro Henrique De Lima <79190116+Pedroo13@users.noreply.github.com>
2024-04-25 19:11:52 -0300

    ajustes

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,12 +1,9 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosListagemMedico;
-import med.voll.api.paciente.*;
+import med.voll.api.domain.paciente.*;
+import med.voll.api.domain.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
